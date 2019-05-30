a3387f8a0ddbee5d9d831844bca15f59997c1089
tamaz bagdavadze <t.bagdavadze@betsolutions.com>
2019-05-30 14:46:31 +0400

    refactoring merchant auth info

--- Betsolutions.Casino.SDK/MerchantAuthInfo.cs
+++ Betsolutions.Casino.SDK/MerchantAuthInfo.cs
@@ -2,8 +2,21 @@
 {
     public class MerchantAuthInfo
     {
-        public int MerchantId { get; set; }
-        public string PrivateKey { get; set; }
-        public string BaseUrl { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="merchantId">Merchant Id</param>
+        /// <param name="privateKey">Merchant's private key</param>
+        /// <param name="baseUrl">API base url with format: "https://example.com/"</param>
+        public MerchantAuthInfo(int merchantId, string privateKey, string baseUrl)
+        {
+            MerchantId = merchantId;
+            PrivateKey = privateKey;
+            BaseUrl = baseUrl;
+        }
+
+        public int MerchantId { get; }
+        public string PrivateKey { get; }
+        public string BaseUrl { get; }
     }
 }
