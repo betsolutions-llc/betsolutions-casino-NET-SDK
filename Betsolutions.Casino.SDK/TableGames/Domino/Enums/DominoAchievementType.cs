3b6d9eb548771206ce7472984f4b960f62ef11b9
tamaz bagdavadze <t.bagdavadze@betsolutions.com>
2019-07-01 16:17:00 +0400

    fix domino enums

--- Betsolutions.Casino.SDK/TableGames/Domino/Enums/DominoAchievementType.cs
+++ Betsolutions.Casino.SDK/TableGames/Domino/Enums/DominoAchievementType.cs
@@ -2,12 +2,7 @@
 {
     public enum DominoAchievementType
     {
-        Roll = 1,
-        WinGame = 2,
-        TournamentWin = 3,
-        Mars = 4,
-        DoubleMars = 5,
-        RoyalDefense = 6,
-        StoneKiller = 7
+        WinGame = 1,
+        TournamentWin = 2,
     }
 }
